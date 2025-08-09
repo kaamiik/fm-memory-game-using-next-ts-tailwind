5fa70f381c1976bb16db5c0f602a1b5538f579e6
Kamran Kiani <kiani.km@gmail.com>
2025-08-09 10:10:56 +0330

    Add PageHeader component
    Introduces a new PageHeader component with navigation, menu, and game control buttons. Includes responsive design for mobile, tablet, and desktop, and exports the component for use in other parts of the application.

---
+++ src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import GameButton from "../GameButton";
+
+function PageHeader() {
+  const dialogRef = React.useRef<HTMLDialogElement>(null);
+
+  function openDialog() {
+    dialogRef.current?.showModal();
+  }
+
+  function closeDialog() {
+    dialogRef.current?.close();
+  }
+
+  return (
+    <header className="p-6 sm:px-10 sm:py-9 lg:p-16 xl:px-0 bg-gray-lighter">
+      <div className="max-w-[69.375rem] mx-auto flex flex-wrap items-center justify-between">
+        <Link
+          className="focus-visible:outline-2 focus-visible:outline-blue-500 focus-visible:outline-offset-2 focus-visible:outline-dotted"
+          href="/"
+        >
+          <Image
+            alt=""
+            src="/assets/logo.svg"
+            width={92}
+            height={30}
+            className="sm:w-[153px] h-auto"
+          />
+          <p className="sr-only">MEMORY GAME SITE</p>
+        </Link>
+        {/* Mobile button */}
+        <GameButton
+          type="button"
+          className="py-2.5 px-[1.125rem] bg-yellow text-400 text-gray-lighter hover:bg-yellow-light focus-visible:bg-yellow-light sm:hidden"
+          onClick={openDialog}
+        >
+          Menu
+        </GameButton>
+        {/* Tablet & Desktop buttons */}
+        <div className="hidden sm:flex gap-4 items-center">
+          <GameButton
+            type="button"
+            className="py-3.5 px-7 bg-yellow text-500 text-gray-lighter hover:bg-yellow-light focus-visible:bg-yellow-light"
+          >
+            Restart
+          </GameButton>
+          <GameButton
+            type="button"
+            className="py-3.5 px-6 bg-blue-light text-500 text-blue-dark hover:bg-blue-medium hover:text-gray-lighter focus-visible:bg-blue-medium focus-visible:text-gray-lighter"
+          >
+            New Game
+          </GameButton>
+        </div>
+      </div>
+      <dialog
+        className="w-full p-6 text-blue-dark text-450 flex flex-col gap-4 rounded-[10px] mx-auto my-auto backdrop:bg-black backdrop:opacity-50"
+        ref={dialogRef}
+      >
+        <GameButton
+          autoFocus
+          className="w-full p-3 rounded-full bg-blue-lighter focus-visible:bg-yellow focus-visible:text-gray-lighter"
+        >
+          Restart
+        </GameButton>
+        <GameButton className="w-full p-3 rounded-full bg-blue-lighter">
+          New Game
+        </GameButton>
+        <GameButton
+          className="w-full p-3 rounded-full bg-blue-lighter"
+          onClick={closeDialog}
+        >
+          Resume Game
+        </GameButton>
+      </dialog>
+    </header>
+  );
+}
+
+export default PageHeader;
