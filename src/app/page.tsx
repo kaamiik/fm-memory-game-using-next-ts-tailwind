4cb463561dc6a3c7f976247bbef1c2ba2b11fee6
Kamran Kiani <kiani.km@gmail.com>
2025-08-09 10:10:05 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
 "use client";
-
 import React from "react";
 
 import GameSetting from "@/components/GameSetting";
 import GameStart from "@/components/GameStart";
+import useIsGameStarted from "@/hooks/useIsGameStarted";
 
 function Home() {
+  const isGameStarted = useIsGameStarted();
+
   return (
     <>
       <React.Suspense fallback={null}>
         <GameStart />
       </React.Suspense>
-      <main className="bg-blue-darker min-h-dvh p-6 sm:py-12 flex flex-col gap-11 sm:gap-[4.6875rem] justify-center">
-        <h1 className="text-700 sm:text-800 text-gray-lighter text-center">
-          memory<span className="sr-only"> game</span>
-        </h1>
-        <GameSetting />
-      </main>
+      {!isGameStarted && (
+        <main className="bg-blue-darker min-h-dvh p-6 sm:py-12 flex flex-col gap-11 sm:gap-[4.6875rem] justify-center">
+          <h1 className="text-700 sm:text-800 text-gray-lighter text-center">
+            memory<span className="sr-only"> game</span>
+          </h1>
+          <GameSetting />
+        </main>
+      )}
     </>
   );
 }
