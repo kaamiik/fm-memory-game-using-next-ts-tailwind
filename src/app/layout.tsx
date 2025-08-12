32f0d2b7473d30d6f7604cf66fa92f3b65aca7f4
Kamran Kiani <kiani.km@gmail.com>
2025-08-12 18:26:44 +0330

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${atkinson.className} antialiased text-300 md:text-450 text-blue-muted`}
+        className={`${atkinson.className} antialiased text-300 md:text-450 text-blue-muted bg-gray-lighter`}
       >
         {children}
       </body>
