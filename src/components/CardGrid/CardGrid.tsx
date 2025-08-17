2b1400c0e6a5d3f6f6dd496dd241c687ee82d619
Kamran Kiani <kiani.km@gmail.com>
2025-08-17 19:03:48 +0330

    Refactor CardGrid to render dynamic cards
    CardGrid now accepts a cards array and onCardClick handler as props, rendering CardButton components based on card data. This enables dynamic display and interaction with cards, supporting flipped and matched states.

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import CardButton from "../CardButton";
 
+import type { Card } from "@/types/gameTypes";
+
 type CardGridProps = {
   gridSize: "4x4" | "6x6";
+  cards: Card[];
+  onCardClick: (card: Card) => void;
 };
 
-function CardGrid({ gridSize }: CardGridProps) {
+function CardGrid({ gridSize, cards, onCardClick }: CardGridProps) {
   const is4x4 = gridSize === "4x4";
 
   const gridConfig = is4x4
@@ -30,11 +34,16 @@ function CardGrid({ gridSize }: CardGridProps) {
     <div
       className={`grid mx-auto ${gridConfig.cols} ${gridConfig.gap} ${gridConfig.maxWidth} ${gridConfig.margin}`}
     >
-      {Array.from({ length: gridConfig.buttonCount }, (_, index) => (
+      {cards.map((card) => (
         <CardButton
-          key={index}
-          className={`${gridConfig.fontSize}`}
-        ></CardButton>
+          key={card.id}
+          className={gridConfig.fontSize}
+          isFlipped={card.isFlipped}
+          isMatched={card.isMatched}
+          onClick={() => onCardClick(card)}
+        >
+          {card.isFlipped || card.isMatched ? card.value : ""}
+        </CardButton>
       ))}
     </div>
   );
